1d346a235acb2006a48e07bd30990bb87e0cde5d
Ben <bswisa@yahoo.com>
2025-12-28 20:01:31 +0200

    Adding USER_DATA to install docker compose

--- infra/main.tf
+++ infra/main.tf
@@ -93,10 +93,15 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              yum update -y
-              yum install -y docker git
-              systemctl enable docker
-              systemctl start docker
+              sudo dnf update -y
+              sudo dnf install -y docker git
+              sudo systemctl enable docker
+              sudo systemctl start docker
+
+              sudo mkdir -p /usr/local/lib/docker/cli-plugins
+              sudo curl -SL "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64" -o /usr/local/lib/docker/cli-plugins/docker-compose
+              sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
               EOF
 
   tags = {
